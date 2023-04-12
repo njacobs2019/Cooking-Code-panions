793bc125101bdf50fdf4cc6844999cd98b3c4d19
Nicholas Jacobs <nicholas.jacobs@maine.edu>
2023-04-12 14:03:31 -0400

    Cleaned app.py, created additional recipe route

--- Flask/app.py
+++ Flask/app.py
@@ -6,20 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def tasks():
-
-    #Uncomment when db is working for everyone. -Tyler
-    #db = DBAccess()
-    #recipe = db.get_random_recipe()
-    #return render_template("recipe_page.html", recipe=recipe)
-
-    #For now you can just render individual templates you've made with this. -Tyler
     return render_template("bigSearch.html")
 
-# @app.route("/add", methods=["GET", "POST"])
-# def add():
-#     if request.method == "GET":
-#         return render_template("add.html")
-#     else:
-#         todo = request.form.get("task")
-#         todos.append(todo)
-#         return redirect("/")
+@app.route("/recipe")
+def recipe():
+    db = DBAccess()
+    recipe = db.get_random_recipe()
+    return render_template("recipe_page.html", recipe=recipe)
+    
