41f3ef6e43b2bccf2615839a2eaa0f093ca9e113
Nicholas Jacobs <nicholas.jacobs@maine.edu>
2023-04-14 20:09:59 +0000

    Deleted Node backend, react frontend, and dockerfiles and compose.

--- backend/src/Actions/actionType.js
+++
@@ -1,9 +0,0 @@
-//Cooking Companion
-//COS420 Group 2
-
-export const ADD_RECIPE= "ADD_RECIPE"
-export const ADD_INGREDIENT= "ADD_INGREDIENT"
-export const ADD_STEP= "ADD_STEP"
-export const ADD_BASIC_INFO= "ADD_BASIC_INFO"
-export const DELETE_RECIPE= "DELETE_RECIPE"
-export const EDIT_RECIPE= "EDIT_RECIPE"
