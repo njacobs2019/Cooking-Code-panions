c01e5b832d5e02550b64c81d427c050cd52708dd
tKharvud <tyler.harwood@maine.edu>
2023-04-24 16:17:58 -0400

    unit tests

--- Flask/tests/db_access.py
+++ Flask/tests/db_access.py
@@ -11,28 +11,35 @@ class testAuthentication(unittest.TestCase):
     #   insert it into the db, grab it by id, then verify the id we gave it matches.
     def test_get_recipe_by_id(self):
 
+        #For a recipe that does exist
         identifier = "6439ee7a028ed862258322b8"
         result = self.db.get_recipe_by_id(identifier)
-        #print(result)
         self.assertEquals(result["_id"], identifier, f"Should be {identifier}")
 
-    def test_get_recipe_by_id(self):
-
-        identifier = "6439ee7a028ed862258322b1"
+        #For a recipe that does not exist
+        identifier = "6439ee7a028ed862258322c2"
         result = self.db.get_recipe_by_id(identifier)
-        self.assertEquals(result["_id"]), identifier, f
+        self.assertEquals(type(result), type(None), f"Should be {type(None)}")
+
 
     # Dependant on get_recipe_by_id()
+    #Verifies that 
     def test_full_search(self):
 
         known = self.db.get_recipe_by_id("6439ee7a028ed862258322b8")
-        results = self.db.full_search("blueberries")
+        results = self.db.full_search(known["title"])
+
+        tokens = known["title"].split()
+        containsKnown = False
     
         #Verify that full_search returns a list of recipe objects
-        #print(type(results))
         for result in results:
-            #print(result["title"])
             self.assertEquals(type(result), type(known), f"Should be {type(known)}")
+
+            if result["title"] == known["title"]:
+                containsKnown = True
+
+        self.assertTrue(constainsKnown)
 
     def test_recipe_display(self, page):
         recipe = self.db.get_recipe_by_id()
