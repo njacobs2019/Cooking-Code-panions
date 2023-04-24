b762613b381f21f8ad92a10842f21466d3546936
tKharvud <tyler.harwood@maine.edu>
2023-04-24 13:46:07 -0400

    d

--- Flask/tests/db_access.py
+++ Flask/tests/db_access.py
@@ -9,15 +9,21 @@ class testAuthentication(unittest.TestCase):
     # Just need to know whether a recipe object has its URI as an attribute
     #   If we don't want to do that the alternative is to create the object here, 
     #   insert it into the db, grab it by id, then verify the id we gave it matches.
-    def test_get_recipe_by_id():
+    def test_get_recipe_by_id(self):
 
         identifier = "6439ee7a028ed862258322b8"
         result = self.db.get_recipe_by_id(identifier)
         #print(result)
-        assertEquals(result["_id"], identifier, f"Should be {identifier}")
+        self.assertEquals(result["_id"], identifier, f"Should be {identifier}")
+
+    def test_get_recipe_by_id(self):
+
+        identifier = "6439ee7a028ed862258322b1"
+        result = self.db.get_recipe_by_id(identifier)
+        self.assertEquals(result["_id"]), identifier, f
 
     # Dependant on get_recipe_by_id()
-    def test_full_search():
+    def test_full_search(self):
 
         known = self.db.get_recipe_by_id("6439ee7a028ed862258322b8")
         results = self.db.full_search("blueberries")
@@ -26,12 +32,12 @@ class testAuthentication(unittest.TestCase):
         #print(type(results))
         for result in results:
             #print(result["title"])
-            assertEquals(type(result), type(known), f"Should be {type(known)}")
+            self.assertEquals(type(result), type(known), f"Should be {type(known)}")
 
     def test_recipe_display(self, page):
         recipe = self.db.get_recipe_by_id()
         #display =        #1. import html as some kind of object
 
         #2. grab attribute of that object and test against attribute in display
-        #assertEquals(type(result), type(known), f"Should be {type(known)}")
+        #self.assertEquals(type(result), type(known), f"Should be {type(known)}")
     
